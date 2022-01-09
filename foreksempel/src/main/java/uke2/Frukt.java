8988a3e2fb28cca90e16e19e98364d978706da61
Børge Haugset <borgeha@ntnu.no>
2022-01-09 14:38:36 +0100

    Div

--- foreksempel/src/main/java/uke2/Frukt.java
+++ foreksempel/src/main/java/uke2/Frukt.java
@@ -19,14 +19,13 @@ public double getAntall() {
 
 public String getType() {
     return type;
+		
 }
-
 
 public static void main(String[] args) {
     Frukt f1 = new Frukt("Appelsin", 2);
 		Frukt f2 = new Frukt("Banan", 1);
-
-		System.out.println("Skrive ut objektreferanser:"); // For å slippe å skrive alt det her: skriv sys, og så trykker du på kontroll-mellomrom.
+		System.out.println("Skrive ut objektreferanser:"); 
 		// Følgende to utskrifter ser ikke helt bra ut. Det skrives ut hvilket objekt hver variabel er.
 		// Legg merke til at begge er uke2.Frukt. Kjør programmet en gang til, og se at tegnene etter @ endrer seg.
 		System.out.println(f1); 
@@ -36,7 +35,7 @@ public static void main(String[] args) {
 		// For å se det faktiske inneholdet av objektene må vi spørre etter dem (vi kan også gjør frukten mer presentabel, men det blir senere!)
 		System.out.println(f1.getType() + " : "+f1.getAntall());
 		System.out.println(f2.getType() + " : "+f2.getAntall());
-
+	
 }
 
 }
