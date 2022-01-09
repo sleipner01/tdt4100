3921911857d6031607d9ce986dcdff96efdcf112
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2022-01-09 22:15:11 +0100

    example

---
+++ minegenkode/src/main/java/javaCrashCourse/Reference.java
@@ -0,0 +1,10 @@
+package javaCrashCourse;
+
+import java.util.Date;
+
+public class Reference {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
