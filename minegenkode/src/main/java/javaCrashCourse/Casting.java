7dfef853cba5ab39844b5d4596dc826aaa0a3051
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-10 15:13:00 +0100

    Casting

---
+++ minegenkode/src/main/java/javaCrashCourse/Casting.java
@@ -0,0 +1,9 @@
+package javaCrashCourse;
+
+public class Casting {
+    public static void main(String[] args) {
+        short x = 1;
+        int y = x + 2;
+        System.out.println(y);
+    }
+}
