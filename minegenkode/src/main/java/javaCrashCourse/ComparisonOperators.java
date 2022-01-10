f917d6eb894fe0d5932cdc7f3d227796bbb189f7
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-10 16:45:35 +0100

    comparison/logical

---
+++ minegenkode/src/main/java/javaCrashCourse/ComparisonOperators.java
@@ -0,0 +1,9 @@
+package javaCrashCourse;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y); // != >= <=
+    }
+}
