191a047dcfdc16ff5cbf4bcb72919c723c227d23
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-10 17:40:26 +0100

    foreach

--- minegenkode/src/main/java/javaCrashCourse/ForEachLoops.java
+++ minegenkode/src/main/java/javaCrashCourse/ForEachLoops.java
@@ -2,6 +2,10 @@ package javaCrashCourse;
 
 public class ForEachLoops {
     public static void main(String[] args) {
-        
+        String[] fruits = { "Apple", "Mango", "Orange" };
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
     }
 }
