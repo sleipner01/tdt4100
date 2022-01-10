bd128ccb4c006307279f0db389d76dbbb4bc351d
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-10 17:24:15 +0100

    for

--- minegenkode/src/main/java/javaCrashCourse/ForLoops.java
+++ minegenkode/src/main/java/javaCrashCourse/ForLoops.java
@@ -2,6 +2,8 @@ package javaCrashCourse;
 
 public class ForLoops {
     public static void main(String[] args) {
+        for(int i = 0; i < 5; i++)
+            System.out.println("Hello World");
         
     }
 }
