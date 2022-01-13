79cbd5e9d6a0c0d77b6cf265474963bbb12e202b
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-13 11:47:51 +0100

    Banking project template

---
+++ minegenkode/src/main/java/byrkjeBank/ByrkjeBank.java
@@ -0,0 +1,15 @@
+// https://youtu.be/hqY6rKhRBzU
+
+package byrkjeBank;
+
+public class ByrkjeBank {
+
+	public static void main(String[] args) {
+				
+		Account beyonce = new Account("Beyonce", "B00002");
+		beyonce.showMenu();
+		
+		
+	}
+
+}
