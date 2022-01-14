dda792690a7820c28d0def95fd0b125ec80592b5
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-14 20:28:09 +0100

    Location.java done

--- ovinger/src/main/java/stateandbehavior/Location.java
+++ ovinger/src/main/java/stateandbehavior/Location.java
@@ -2,4 +2,46 @@ package stateandbehavior;
 
 public class Location {
     
+    int x;
+    int y;
+
+    public Location() {
+        x = 0;
+        y = 0;
+    }
+
+    public void up() {
+        y--;
+    }
+
+    public void down() {
+        y++;
+    }
+
+    public void left() {
+        x--;
+    }
+
+    public void right() {
+        x++;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public static void main(String[] args) {
+        Location location = new Location();
+        System.out.println(location);
+    }
+
+    @Override
+    public String toString() {
+        return "[xPos: " + x + " | yPos: " + y + "]";
+    }
+
 }
