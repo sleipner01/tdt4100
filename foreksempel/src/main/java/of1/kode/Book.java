76d947c56d8dbe5893997a19797123892ef51ced
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-21 09:58:44 +0100

    of1 Book.java

--- foreksempel/src/main/java/of1/kode/Book.java
+++ foreksempel/src/main/java/of1/kode/Book.java
@@ -2,20 +2,49 @@ package of1.kode;
 
 public class Book {
 
+    String title;
+    int numPages;
+
+    public Book(int numPages, String title) {
+        this.title = title;
+        this.numPages = numPages;
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getNumPages() {
+        return this.numPages;
+    }
+
     public static void main(String[] args) {
-        /*
-         * Book book = new Book(100, "Big Java");
-         * 
-         * System.out.println("The book \"" + book.getTitle() + "\" has " +
-         * book.getNumPages() + " pages.");
-         * 
-         * book.setNumPages(718);
-         * book.setTitle("Introduction to Algorithms");
-         * System.out.println("The book \"" + book.getTitle() + "\" has " +
-         * book.getNumPages() + " pages.");
-         * 
-         * System.out.println(book);
-         */
+        
+        Book book = new Book(100, "Big Java");
+        
+        System.out.println(book);
+        
+        book.setNumPages(718);
+        book.setTitle("Introduction to Algorithms");
+        System.out.println(book);
+        
+        System.out.println(book);
+        
+    }
+
+    @Override
+    public String toString() {
+        return "The book \"" + this.getTitle() + "\" has " +
+        this.getNumPages() + " pages.";
     }
 
 }
