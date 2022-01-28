05dbd8ccdffe302b1f4d4c844e81eb89fc616df0
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-28 10:12:31 +0100

    ovingtime

--- minegenkode/src/main/java/ovingTimeOppgaver/SelfServiceCheckout.java
+++ minegenkode/src/main/java/ovingTimeOppgaver/SelfServiceCheckout.java
@@ -19,6 +19,42 @@ public class SelfServiceCheckout {
             throw new IllegalArgumentException("Invalid weekday");
     }
 
+    public void registerPhoneNumber(String phoneNumber) {
+        if(!isValidPhoneNumber(phoneNumber)) {
+            throw new IllegalArgumentException("This is not a valid norwegian phone number.");
+        }
+
+        this.phoneNumber = phoneNumber;
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        String cleanedPhoneNumber = phoneNumber.replaceAll("\\s.", "");
+
+        if(cleanedPhoneNumber.startsWith("00479") || cleanedPhoneNumber.startsWith("00474")) {
+            if(cleanedPhoneNumber.length() != 12) return false;
+        }
+        else if(cleanedPhoneNumber.startsWith("+479") || cleanedPhoneNumber.startsWith("+474")) {
+            if(cleanedPhoneNumber.length() != 11) return false;
+        }
+        else return false;
+
+        char[] chars = cleanedPhoneNumber.toCharArray();
+        boolean isFirstIndex = true;
+        for (char c : chars) {
+            if (c == '+' && isFirstIndex) {
+                isFirstIndex = false;
+                continue;
+            }
+            if (!Character.isDigit(c)) return false;
+        }
+
+        return true;
+    }
+
+    public boolean scanItem(String product, int price, double discount) {
+        return true;
+    }
+
     public static void main(String[] args) {
         
     }
