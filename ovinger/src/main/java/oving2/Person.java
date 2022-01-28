d73fb967cc13a345da08eff7cdd37f3fa7c8c6c5
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-28 12:58:06 +0100

    email validation

--- ovinger/src/main/java/oving2/Person.java
+++ ovinger/src/main/java/oving2/Person.java
@@ -1,13 +1,42 @@
 package oving2;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Person {
 
     private String name;
-    private String Email;
+    private String email;
     private Date birthday;
     private char gender = '\0';
+    
+    private static final String emailFormat = "^[A-Za-z.]+@[A-Za-z.]+$";
+    private static final List<String> landCodes = Arrays.asList(
+        "ad", "ae", "af", "ag", "ai", "al", "am", "ao", "aq", "ar", "as",
+        "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg",
+        "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt",
+        "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci",
+        "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy",
+        "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh",
+        "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb",
+        "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq",
+        "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht",
+        "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it",
+        "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp",
+        "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls",
+        "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh",
+        "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu",
+        "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni",
+        "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg",
+        "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa",
+        "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg",
+        "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st",
+        "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk",
+        "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug",
+        "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu",
+        "wf", "ws", "ye", "yt", "za", "zm", "zw");
 
 
     public String getName() {
@@ -15,7 +44,7 @@ public class Person {
     }
 
     public String getEmail() {
-        return this.Email;
+        return this.email;
     }
 
     public Date getBirthday() {
@@ -56,8 +85,26 @@ public class Person {
         this.name = name;
     }
 
-    public void setEmail(String Email) {
-        this.Email = Email;
+    private boolean validateEmail(String email) {
+
+        if(!Pattern.matches(emailFormat, email))
+            throw new IllegalArgumentException("Your email pattern is wrong.");
+             
+        String[] nameArray = this.name.split(" ");
+        String emailPrefix = nameArray[0].toLowerCase() + "." + nameArray[1].toLowerCase();
+        if(!email.startsWith(emailPrefix))
+        throw new IllegalArgumentException("The email must start with: " + emailPrefix);
+        
+        String[] emailParts = email.split("[.]");
+        if(!landCodes.contains(emailParts[emailParts.length - 1]))
+            throw new IllegalArgumentException("Invalid landcode.");
+
+        return true;
+    }
+
+    public void setEmail(String email) {
+        validateEmail(email);
+        this.email = email;
     }
 
     private boolean valiDate(Date date) throws IllegalAccessException {
@@ -99,11 +146,14 @@ public class Person {
 
     public static void main(String[] args) {
 
+        Person person = new Person();
+        person.setEmail("ola.nordmann@ntnu");
+
     }
 
     @Override
     public String toString() {
-        return "Person [Email=" + Email + ", birthday=" + birthday + ", name=" + name + ", gender=" + gender + "]";
+        return "Person [Email=" + email + ", birthday=" + birthday + ", name=" + name + ", gender=" + gender + "]";
     }
 
 }
