061278fb047d3a57d6ea610ec404c665c3534b3b
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-01-28 09:21:55 +0100

    Private, name, birthday validation

--- ovinger/src/main/java/oving2/Person.java
+++ ovinger/src/main/java/oving2/Person.java
@@ -7,7 +7,7 @@ public class Person {
     private String name;
     private String Email;
     private Date birthday;
-    private char sex;
+    private char gender = '\0';
 
 
     public String getName() {
@@ -22,18 +22,37 @@ public class Person {
         return this.birthday;
     }
 
-    public char getSex() {
-        // if (sex == '\0') return '\0';
-        return this.sex;
+    public char getGender() {
+        return this.gender;
     }
 
-    private boolean validateName(String name, boolean throwExeption) {
-    
+    private boolean validateName(String name) throws IllegalArgumentException {
+        if (!name.contains(" "))
+            throw new IllegalArgumentException("You need to write both a first-name and sur-name, and split them with a space.");
+
+        if (!name.matches("[a-zA-Z ]+")) 
+            throw new IllegalArgumentException("The name has to contain valid characters from the alphabet.");
+
+        
+        String[] nameArray = name.split(" ");
+        if(nameArray.length > 2)
+            throw new IllegalArgumentException("The name can only contain a first-name and a sur-name.");
+        if(nameArray[0].length() < 2)
+            throw new IllegalArgumentException("The first-name need to have at least two characters.");
+        if(nameArray[1].length() < 2)
+            throw new IllegalArgumentException("The sur-name need to have at least two characters.");
+        
+        System.out.println(nameArray[0]);
+        System.out.println(nameArray[1]);
+
+
+
         return true;
     }
 
-    public void setName(String name, boolean throwExeption) {
-        validateName(name, throwExeption);
+    public void setName(String name) throws IllegalArgumentException {
+        name = name.trim();
+        validateName(name);
         this.name = name;
     }
 
@@ -41,27 +60,28 @@ public class Person {
         this.Email = Email;
     }
 
-    private boolean valiDate(Date date, boolean throwExeption) {
+    private boolean valiDate(Date date) throws IllegalAccessException {
         Date today = new Date();
 
         if(today.before(date)) {
-            if(throwExeption)
-                throw new IllegalArgumentException("Cannot set a future date");
-
-            return false;
+            //if(throwExeption)
+            throw new IllegalArgumentException("Cannot set a future date");
         }
         
         return true;
     }
 
-    public void setBirthday(Date birthday, boolean throwExeption) {
-        valiDate(birthday, throwExeption);
+    public void setBirthday(Date birthday) throws IllegalAccessException {
+        valiDate(birthday);
         this.birthday = birthday;
     }
 
-    private boolean validateSex(char sex, boolean throwExeption) {
+    private boolean validateGender(char genderInput, boolean throwExeption) {
+        // if (genderInput == 'm' || genderInput == 'f') {
+        //     genderInput = Character.toUpperCase(genderInput);
+        // }
 
-        if (sex != 'M' || sex != 'F') {
+        if (genderInput != 'M' || genderInput != 'F') {
             if (throwExeption)
                 throw new IllegalArgumentException("The gender can only be F or M.");
             
@@ -71,19 +91,19 @@ public class Person {
         return true;
     }
 
-    public void setSex(char sex) {
-        validateSex(sex, true);
-        this.sex = sex;
+    public void setGender(char gender) {
+        validateGender(gender, true);
+        this.gender = gender;
     }
 
 
     public static void main(String[] args) {
-        
+
     }
 
     @Override
     public String toString() {
-        return "Person [Email=" + Email + ", birthday=" + birthday + ", name=" + name + ", sex=" + sex + "]";
+        return "Person [Email=" + Email + ", birthday=" + birthday + ", name=" + name + ", gender=" + gender + "]";
     }
 
 }
