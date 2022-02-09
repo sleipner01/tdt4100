d396cf403ecf87fdf9d639a922fc7c304d2ed21d
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-02-09 13:47:34 +0100

    yeah

--- ovinger/src/main/java/oving4/twitter/TwitterAccount.java
+++ ovinger/src/main/java/oving4/twitter/TwitterAccount.java
@@ -1,5 +1,61 @@
 package oving4.twitter;
 
 public class TwitterAccount {
+
+    private String userName;
+
+    public TwitterAccount(String username) {
+
+        if(isValidUserName(username)) this.userName = username;
+    }
+
+    private boolean isValidUserName(String username) {
+
+
+        return true;
+    }
+
+    public String getUserName() {
+
+        return this.userName;
+    }
+
+    public void follow(TwitterAccount account) {
+
+    }
     
+    public void unfollow(TwitterAccount account) {
+
+    }
+
+    public boolean isFollowing(TwitterAccount account) {
+
+        return false;
+    }
+
+    public boolean isFollowedBy(TwitterAccount account) {
+     
+        return false;
+    }
+
+    public void tweet(String text) {
+
+    }
+
+    public void retweet(Tweet tweet) {
+
+    }
+
+    public Tweet getTweet(int i) {
+        if(!isValidTweetId(i))
+            throw new IllegalArgumentException("Not valid id for tweet.");
+        
+        return tweet;
+    }
+
+    private boolean isValidTweetId(int i) {
+        
+        return true;
+    }
+
 }
