31231de9529ab715c423a4c65e2c96022b3d3fca
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-02-13 21:25:21 +0100

    testing CoffeeCup

--- ovinger/src/test/java/oving4/testing/CoffeeCupTest.java
+++ ovinger/src/test/java/oving4/testing/CoffeeCupTest.java
@@ -1,5 +1,29 @@
 package oving4.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+ 
+import org.junit.jupiter.api.Test;
+
 public class CoffeeCupTest {
+
+    CoffeeCup coffeeCup = new CoffeeCup();
+    CoffeeCup coffeeCup2 = new CoffeeCup(40, 0);
+
+
+    @Test
+    public void testGetCapacity() {
+        assertEquals(0.0, coffeeCup.getCapacity());
+        assertEquals(40, coffeeCup2.getCapacity());
+    }
+
+    @Test
+    public void testGetCurrentVolume() {
+        assertEquals(0.0, coffeeCup.getCurrentVolume());
+        assertEquals(0, coffeeCup2.getCurrentVolume());
+    }
+
+
     
 }
