0ca1b433282e49c33bc3319b08f8aa7554a0f73b
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-02-13 23:09:18 +0100

    CoffeeCupTest done

--- ovinger/src/test/java/oving4/testing/CoffeeCupTest.java
+++ ovinger/src/test/java/oving4/testing/CoffeeCupTest.java
@@ -2,6 +2,7 @@ package oving4.testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
  
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,32 @@ public class CoffeeCupTest {
         assertEquals(0, coffeeCup2.getCurrentVolume());
     }
 
+    @Test
+    public void testIncreaseCupSize() {
+        coffeeCup.increaseCupSize(20);
+        assertEquals(20, coffeeCup.getCapacity());
+    }
 
+    @Test
+    public void testFillCoffee() {
+        assertThrows(RuntimeException.class, () -> {
+			coffeeCup.fillCoffee(10);
+		}, "Skal ikke kunne fylle på mere kaffe enn koppen rommer");
+
+        coffeeCup2.fillCoffee(40);
+        assertEquals(40, coffeeCup2.getCurrentVolume());
+    }
+
+    @Test
+    public void testDrinkCoffee() {
+        assertThrows(RuntimeException.class, () -> {
+			coffeeCup.drinkCoffee(10);
+		}, "Skal ikke kunne drikke mere kaffe enn det finnes i koppen");
+
+        coffeeCup2.fillCoffee(10);
+        assertEquals(10, coffeeCup2.getCurrentVolume());
+        coffeeCup2.drinkCoffee(10);
+        assertEquals(0, coffeeCup2.getCurrentVolume());
+    }
     
 }
