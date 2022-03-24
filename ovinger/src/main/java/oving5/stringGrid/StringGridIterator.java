7809f3a2f023fbaa52754fc770a40560a207371c
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-03-24 14:22:20 +0100

    SGIterator file prepared

---
+++ ovinger/src/main/java/oving5/stringGrid/StringGridIterator.java
@@ -0,0 +1,19 @@
+package oving5.stringGrid;
+
+import java.util.Iterator;
+
+public class StringGridIterator implements Iterator<String> {
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
