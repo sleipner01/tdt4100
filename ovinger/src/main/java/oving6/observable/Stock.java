8b6e6409b88e6e02e76ff4e96ff88d3c511dc51c
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-04-01 15:21:56 +0200

    Stock done

--- ovinger/src/main/java/oving6/observable/Stock.java
+++ ovinger/src/main/java/oving6/observable/Stock.java
@@ -1,5 +1,40 @@
 package oving6.observable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
-    
+
+    private String ticker;
+    private double sharePrice;
+    private List<StockListener> listeners;
+
+    public Stock(String ticker, double sharePrice) {
+        this.ticker = ticker;
+        this.sharePrice = sharePrice;
+        this.listeners = new ArrayList<>();
+    }
+
+    public void setPrice(double sharePrice) {
+        if(sharePrice <= 0) 
+            throw new IllegalArgumentException("The price has to be above 0.");
+        this.listeners.forEach(listener -> listener.stockPriceChanged(this, this.getPrice(), sharePrice));
+        this.sharePrice = sharePrice;
+    }
+
+    public String getTicker() {
+        return this.ticker;
+    }
+
+    public double getPrice() {
+        return this.sharePrice;
+    }
+
+    public void addStockListener(StockListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeStockListener(StockListener listener) {
+        listeners.remove(listener);
+    }
 }
--- ovinger/src/main/java/oving6/observable/StockListener.java
+++ ovinger/src/main/java/oving6/observable/StockListener.java
@@ -1,5 +1,7 @@
 package oving6.observable;
 
 public interface StockListener {
-    
+
+    void stockPriceChanged(Stock stock, double oldValue, double newValue);
+
 }
