1306a43ac074d648f727fb207f7d02c7aaf062e6
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-04-03 21:45:53 +0200

    Minor fixes

--- snakebird-example/src/test/java/snakebird/TileTest.java
+++ snakebird-example/src/test/java/snakebird/TileTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import snakebird.Tile;
-
 public class TileTest {
 
 	private Tile tile;
