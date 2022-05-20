2d672bfacc4a9392506195aa5ec68b702340dd5f
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-05-20 18:34:06 +0200

    cleared for part 2

---
+++ minegenkode/src/main/java/eksamenOving/kode/kont2021/App.java
@@ -0,0 +1,21 @@
+package eksamenOving.kode.kont2021;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Voteshit");
+		primaryStage.setScene(new Scene(FXMLLoader.load(App.class.getResource("UI.fxml"))));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		App.launch(args);
+	}
+
+}
---
+++ minegenkode/src/main/java/eksamenOving/kode/kont2021/AppController.java
@@ -0,0 +1,24 @@
+package eksamenOving.kode.kont2021;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class AppController {
+
+	// TODO - Add any needed fields here
+	@FXML
+	public TextArea output;
+	public static String logInSuccess = "Gratulerer du har logget inn!";
+	public static String logInFailed = "Feil brukernavn eller passord";
+
+	/**
+	 * TODO - what should be the name of this method? Uncomment this part of the
+	 * code and put it inside the correct function public void { String brukernavn;
+	 * // Extract username from FXML here String passord; // Extract password from
+	 * FXML here
+	 * 
+	 * if (brukernavn.equals("admin") && passord.equals("admin")) {
+	 * output.setText(logInSuccess); } else { output.setText(logInFailed); } }
+	 */
+
+}
