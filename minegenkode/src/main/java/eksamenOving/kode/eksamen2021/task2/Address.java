6f831718db14fb6c7f08b5ca8e38338b6ad733e3
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-06-05 13:26:25 +0200

    task 2 done

--- minegenkode/src/main/java/eksamenOving/kode/eksamen2021/task2/Address.java
+++ minegenkode/src/main/java/eksamenOving/kode/eksamen2021/task2/Address.java
@@ -1,5 +1,22 @@
 package eksamenOving.kode.eksamen2021.task2;
 
+// DO NOT MODIFY THIS CLASS
 public class Address {
-    
+ 
+    private String streetName;
+    private int streetNumber;
+ 
+    public Address(String streetName, int streetNumber) {
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+    }
+ 
+    public String getStreetName() {
+        return streetName;
+    }
+ 
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+ 
 }
--- minegenkode/src/main/java/eksamenOving/kode/eksamen2021/task2/Person.java
+++ minegenkode/src/main/java/eksamenOving/kode/eksamen2021/task2/Person.java
@@ -1,5 +1,47 @@
 package eksamenOving.kode.eksamen2021.task2;
 
 public class Person {
-    
+ 
+    // No other fields should be added to this class
+    private Address address;
+ 
+     
+    // You do not need to consider the edge case of passing null into the constructor here
+    public Person(Address address) {
+        this.address = address;
+    }
+     
+    /**
+     *
+     * @return the street name of this person
+     */
+    public String getStreetName() {
+        return this.address.getStreetName();
+    }
+ 
+    /**
+     * Updates the street name of this person
+     *
+     * @param streetName The street name to update
+     */
+    public void setStreetName(String streetName) {
+        this.address = new Address(streetName, this.address.getStreetNumber());
+    }
+     
+     
+    public int getStreetNumber() {
+        return this.address.getStreetNumber();
+    }
+ 
+    /**
+     * Updates the street number of this person
+     *
+     * @param streetNumber A positive integer.
+     *
+     * @throws IllegalArgumentException If number is not larger than 0.
+     */
+    public void setStreetNumber(int streetNumber) {
+        if(streetNumber < 0) throw new IllegalArgumentException("Has to be above 0");
+        this.address = new Address(this.address.getStreetName(), streetNumber);
+    }
 }
